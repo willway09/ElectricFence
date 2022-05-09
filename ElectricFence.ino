823f4cb892f86ad07e4083920d8d7e758b07372b
willway09 <willway@outlook.com>
2022-05-09 17:14:19 -0400

    Add support for ESP8266

--- ElectricFence.ino
+++ ElectricFence.ino
@@ -1,11 +1,15 @@
 #include "data.h"
 
-void setup()
-{
-  Serial.begin(9600);
-  //DDRB = B0000011;
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
+void setup() {
+  #if defined(__AVR_ATmega328P__) //If Arduino Uno, Nano, etc. (ATmega328p-based)
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+  #elif defined(ESP8266) //If ESP8266
+    pinMode(12, OUTPUT);
+    pinMode(13, OUTPUT);
+  #else
+    #error "Unsupported Board Type"
+  #endif
 }
 
 void loop() {
@@ -16,7 +20,14 @@ void loop() {
   
     if(index >= changes) break;
     if(micros() - start >= times[index]) {
-      PORTB = state[index];
+      #if defined(__AVR_ATmega328P__) //If Arduino Uno, Nano, etc. (ATmega328p-based)
+        PORTB = state[index];
+      #elif defined(ESP8266) //If ESP8266
+        //Bit shift 12, because first output pin is 12
+        GPIO_REG_WRITE(GPIO_OUT_ADDRESS, state[index] << 12);
+      #else
+        #error "Unsupported Board Type"
+      #endif
       index++;
     }
   }
--- data.h
+++ data.h
@@ -1,3 +1,4 @@
+#pragma once
 const int changes = 345;
 
 unsigned long times[changes] = {
